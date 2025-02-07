5254ac4f2ac3a35189d8b1c2332233d52ef586af
AlejandroFNadal <alexandronadal@gmail.com>
2025-02-08 00:28:16 +0100

    some progress in malloc

--- linux_programming_interface/memalloc/my_malloc.c
+++ linux_programming_interface/memalloc/my_malloc.c
@@ -30,7 +30,7 @@ void debug_log(const char *msg){
 extern long int etext, edata, end;
 struct free_area {
   uint8_t marker;
-  void *start;
+  struct free_area *prev;
   bool in_use;
   uint32_t length;
   struct free_area *next;
@@ -45,19 +45,56 @@ typedef struct stats my_stats;
 typedef struct free_area area;
 
 const int MAGICAL_BYTES = 0x55AA;
-const int BLOCK_MARKET = 0xDD;
+const int BLOCK_MARKER = 0xDD;
 const int FIRST_BLOCK_OFFSET = sizeof(area);
 
 // Assumes that the magical bytes are at the beginning
-void add_used_block(size_t size){
+int add_used_block(size_t size){
   long int *heap_start = &end;
-  assert(*heap_start == MAGICAL_BYTES);
+  // format the heap_start with the shape of stats
   my_stats *malloc_header = (my_stats*) (heap_start);
-  while(malloc_header->my_simple_lock){};
+  assert(malloc_header->magical_bytes == MAGICAL_BYTES);
+  while(malloc_header->my_simple_lock){
+    sleep(1);
+  };
+  malloc_header->my_simple_lock = true;
   // find smallest space in the free blocks and add there.
-  area *first_block = (area*)(heap_start + FIRST_BLOCK_OFFSET);
-  assert(first_block->marker == BLOCK_MARKET);
-  // increase used block counter
+  area *block = (area*)(heap_start + FIRST_BLOCK_OFFSET);
+  area *smallest_block = NULL;
+  while (block != NULL){
+    assert(block->marker == BLOCK_MARKER);
+    if((block->length + sizeof(block)) >= size && block->in_use == false) {
+      if(smallest_block == NULL || smallest_block->length > block->length){
+        smallest_block == block;
+      }
+    }
+    block = block->next;
+  }
+  // no big enough blocks.
+  if(smallest_block == NULL){
+    sbrk(4096);
+  }
+  // found a block
+  if(smallest_block != NULL){
+    smallest_block->in_use = true;
+    // create a new block, which will be free
+    // only create if the size is big enough
+    int remaining_size = smallest_block->length - size - sizeof(area);
+    if(remaining_size > 0){
+      area *new_block = (area*) smallest_block + sizeof(smallest_block) +smallest_block->length;
+      new_block->marker=BLOCK_MARKER;
+      new_block->prev = smallest_block;
+      new_block->next = smallest_block->next;
+      if(new_block->next != NULL){
+        (new_block->next)->prev = new_block;
+      }
+      smallest_block->next = new_block;
+      new_block->length = smallest_block->size - size
+    }
+    smallest_block->length = size;
+  }
+  malloc_header->my_simple_lock = false; 
+  return smallest_block + sizeof(*free_area);
 }
 void *an_malloc(size_t size) {
   // First, we check if the magical bytes are at the beggining of the heap
@@ -67,19 +104,17 @@ void *an_malloc(size_t size) {
   //printf("Heap end is %p\n", heap_end);
   long int length = heap_end - heap_start;
   //printf("Heap size in KB is %ld\n", length/1024);
-  if ((*heap_start) == MAGICAL_BYTES){
-    add_used_block(size);
-  } else {
+  if ((*heap_start) != MAGICAL_BYTES){
     *(heap_start) = MAGICAL_BYTES; 
     area *first_block = (area*) heap_start + sizeof(my_stats);
-    first_block->start = (heap_start + 2);
-    first_block->in_use = true;
+    first_block->marker = BLOCK_MARKER;
+    first_block->in_use = false;
     first_block->length = size;
     first_block->next = NULL;
+    first_block->prev = NULL;
   }
-  return 0;
+  return add_used_block(size);
 }
-
 int main() {
   //printf("Etext: %p\n", &etext);
   //printf("Edata: %p\n", &edata);
